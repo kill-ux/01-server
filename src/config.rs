33ebc022759bef55f5b0adbedbeb2f311d6af854
killux <mustaphaboutoubdev@gmail.com>
2025-12-09 10:00:48 +0100

    add enum method

--- src/config.rs
+++ src/config.rs
@@ -3,10 +3,24 @@ use std::{error::Error, fs::File, io::Read};
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
+pub enum Method {
+    GET,
+    POST,
+    DELETE
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Route {
+    pub path: String,
+    pub methods: Vec<Method>
+}
+
+#[derive(Deserialize, Debug)]
 pub struct Server {
     pub host: String,
     pub ports: Vec<u16>,
     pub server_name: String,
+    pub routes: Vec<Route>
 }
 
 #[derive(Deserialize, Debug)]
@@ -15,10 +29,12 @@ pub struct Config {
     pub servers: Vec<Server>,
 }
 
-pub fn parse_config() -> Result<Config, Box<dyn Error>> {
-    let mut file = File::open("config.yaml")?;
-    let mut buf = String::new();
-    file.read_to_string(&mut buf)?;
-    let config: Config = serde_yaml::from_str(&buf)?;
-    Ok(config)
+impl Config {
+    pub fn parse() -> Result<Config, Box<dyn Error>> {
+        let mut file = File::open("config.yaml")?;
+        let mut buf = String::new();
+        file.read_to_string(&mut buf)?;
+        let config: Config = serde_yaml::from_str(&buf)?;
+        Ok(config)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use server::config::*;
 
 fn main() {
-    let config = match parse_config() {
+    let config = match Config::parse() {
         Ok(cfg) => cfg,
         Err(err) => {
             println!("Error : {}", err);
